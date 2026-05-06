359b5e50bf843d0fa38ae819e8d7690b10fcebb0
문소현 <munso8432@gmail.com>
2026-05-06 14:30:03 +0900

    Add configuration for watermark methods and language codes

---
+++ config.py
@@ -0,0 +1,13 @@
+WATERMARK_METHODS = ["KGW", "AWT", "SynthID", "Unigram"]
+
+LANG_CODES = {
+    "ZH": "zho_Hans",
+    "DE": "deu_Latn",
+    "JA": "jpn_Jpan",
+    "TH": "tha_Thai",
+    "HI": "hin_Deva",
+    "KO": "kor_Hang",
+    "GRN": "grn_Latn",
+}
+
+MAX_TOKENS = 512
